510bcd1ca721f88c0dc953cf2a53aa237ab6eaa1
manu381m <manu381.m@gmail.com>
2020-06-16 18:11:20 +0530

    added local variable for springapplication run

--- src/main/java/com/example/demo/SpringbootAppApplication.java
+++ src/main/java/com/example/demo/SpringbootAppApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAppApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(SpringbootAppApplication.class, args);
 	}
 
 }
